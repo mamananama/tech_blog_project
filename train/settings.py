8863db760f0f20c8ea1d9b25c6986c027b282e3d
mamanana <blublobla.000@gmail.com>
2023-11-07 22:37:11 +0900

    발표 전  최종 commit

--- train/settings.py
+++ train/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9y%h0s+g+xuv_=ab32vr0ds+_&8h#sy1=+x8dnri8dh08yto2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
